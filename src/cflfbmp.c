37b2fb888df3d41d451eae881f5053fa02d95e6e
mkostoevr <mkostoevr@yandex.ru>
2019-12-21 13:32:12 +0300

    Bugfix: non-right check for pixel y after its modification

--- src/cflfbmp.c
+++ src/cflfbmp.c
@@ -1,10 +1,10 @@
 #include <cflf.h>
 
 COLORREF GetBitmapPixel(FULLBITMAP fbmp, UINT x, UINT y) {
-    y = (fbmp.lHeight - 1) - y;
     // set S value of SRGB structure if x and y are out of bounds
     if (x >= fbmp.lWidth || y >= fbmp.lHeight) {
         return 0xff000000;
     }
+    y = (fbmp.lHeight - 1) - y;
     return *(COLORREF *)&fbmp.pBits[y * (((24 * fbmp.lWidth + 31)&~31) / 8) + x * 3] & 0xffffff;
 }
